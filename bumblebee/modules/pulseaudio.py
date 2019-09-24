b6637a6ed80a482fa99192f3bead4b3afcf2217d
Tobias Witek <tobi@tobi-wan-kenobi.at>
2019-09-24 19:44:59 +0200

    [pulseaudio] Add output logging

--- bumblebee/modules/pulseaudio.py
+++ bumblebee/modules/pulseaudio.py
@@ -128,12 +128,16 @@ class Module(bumblebee.engine.Module):
 
     def volume(self, widget):
         if self._failed == True:
+            logging.debug("output: n/a")
             return "n/a"
         if int(self._mono) > 0:
+            logging.debug("output: mono {}%".format(self._mono))
             return "{}%".format(self._mono)
         elif self._left == self._right:
+            logging.debug("output: {}%".format(self._left))
             return "{}%".format(self._left)
         else:
+            logging.debug("output lr: {}/{}".format(self._left, self._right))
             return "{}%/{}%".format(self._left, self._right)
 
     def update(self, widgets):
